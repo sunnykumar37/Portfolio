1d489a246f039391bcc4637cd459229e0bc1d511
sunnykumar37 <sunnyk28912@gmail.com>
2026-03-11 20:45:13 +0530

    changed pic

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -19,8 +19,8 @@ const Photo = () => {
             opacity: 1,
             transition: { delay: 2.4, duration: 0.4, ease: "easeInOut" }
           }}
-          className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute">
-          <Image src="/MAIN.png"
+          className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] absolute">
+          <Image src="/Mynewpic.png"
             priority
             quality={100}
             fill
